c5b4ced60cbd1c0c0cf35d2e6286d92d5909c46d
seanjamu <seanjamu@hotmail.com>
2023-07-03 13:06:00 -0500

    ok fase 4

---
+++ fase4/script.js
@@ -0,0 +1,23 @@
+function procesar_respuestas(){
+    let total = 5;
+    let puntos = 0;
+
+    let myForm = document.forms['formulario'];
+    let respuestasCorrectas = ["a","b","a","b","c"];
+
+    for (let i = 1; i <= total; i++) {
+        if(myForm["r" + i].value == null ||
+            myForm["r" + i].value == ''){
+            alert("Favor responder todas las preguntas");
+            return false;
+        }else{
+            if (myForm["r" + i].value == respuestasCorrectas[i-1]){
+                puntos++;
+            }
+        }
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = 'Obtuviste ' + puntos + ' puntos de ' + total + ' posibles';
+    return false;
+}
